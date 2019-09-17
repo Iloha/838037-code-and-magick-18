409471b8ba13d552248296944e0696ddaec98e2b
Ilona Khafizova <ilona.khafizova@workato.com>
2019-09-16 22:01:08 -0700

    created stat columns

--- js/stat.js
+++ js/stat.js
@@ -1,6 +1,6 @@
 'use strict';
 
-window.renderStatistics = (function (ctx, names, times) {
+window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 10, 420, 270);
   ctx.fillStyle = 'white';
@@ -10,4 +10,27 @@ window.renderStatistics = (function (ctx, names, times) {
   ctx.textBaseline = 'hanging';
   ctx.fillText('Ура вы победили!', 110, 20);
   ctx.fillText('Список результатов:', 110, 40);
-})
+
+  var max = 0;
+  for (var i = 0; i < times.length; i++) {
+    if(times[i] > max) {
+      max = times[i];
+    }
+  }
+
+  for (var i = 0; i < names.length; i++) {
+    var height = times[i]*150/max;
+    var top = 150 - height;
+    var color;
+    if (names[i] === 'Вы') {
+      color = 'rgba(255, 0, 0, 1)';
+    } else {
+      color = 'hsl(240,100%, 50%)'
+    }
+    ctx.fillStyle = color;
+    ctx.fillRect((130 + i * 90), 90 + top, 40, height);
+    ctx.textBaseline = 'hanging';
+    ctx.fillText(Math.round(times[i]), 130 + i * 90, 70 + top);
+    ctx.fillText(names[i], 130 + i * 90, 245);
+  };
+}
