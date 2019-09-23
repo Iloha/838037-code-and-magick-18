1e07a439b84d65759e32e7797547074a9bc66f76
Ilona Khafizova <ilona.khafizova@workato.com>
2019-09-23 10:38:36 -0700

    moved logic separate functions

--- js/setup.js
+++ js/setup.js
@@ -8,40 +8,49 @@ var surnamesArray = ['да Марья', 'Верон', 'Мирабелла', 'В�
 var coatColorArray = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
 var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
 
+
 var getRandomFromArray = function (array) {
   return array[Math.floor(Math.random() * array.length)];
 };
 
-var heroesArray = [];
-
-for (var i = 0; i < 4; i++) {
-  heroesArray.push({
-    name: getRandomFromArray(namesArray) + ' ' + getRandomFromArray(surnamesArray),
-    coatColor: getRandomFromArray(coatColorArray),
-    eyesColor: getRandomFromArray(eyesColor)
-  });
-}
-
-var wizardsList = document.querySelector('.setup-similar-list');
-var wizardTemplate = document.querySelector('#similar-wizard-template')
-  .content
-  .querySelector('.setup-similar-item');
+var generateWizardsData = function () {
+  var heroesArray = [];
+  for (var i = 0; i < 4; i++) {
+    heroesArray.push({
+      name: getRandomFromArray(namesArray) + ' ' + getRandomFromArray(surnamesArray),
+      coatColor: getRandomFromArray(coatColorArray),
+      eyesColor: getRandomFromArray(eyesColor)
+    });
+  }
+  return heroesArray;
+};
 
 var renderWizard = function (wizard) {
+  var wizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
+
   var wizardElement = wizardTemplate.cloneNode(true);
+
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
   wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
   return wizardElement;
 };
 
-var fragment = document.createDocumentFragment();
+var renderWizardsList = function (array) {
+  var fragment = document.createDocumentFragment();
+  var wizardsList = document.querySelector('.setup-similar-list');
 
-for (var j = 0; j < heroesArray.length; j++) {
-  fragment.appendChild(renderWizard(heroesArray[j]));
-}
+  for (var j = 0; j < array.length; j++) {
+    fragment.appendChild(renderWizard(array[j]));
+  }
+  wizardsList.appendChild(fragment);
+};
 
-wizardsList.appendChild(fragment);
+var wizardsData = generateWizardsData();
+
+renderWizardsList(wizardsData);
 
 var setUpSimilar = document.querySelector('.setup-similar');
 setUpSimilar.classList.remove('hidden');
